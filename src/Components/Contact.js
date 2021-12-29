681655bf50046901f892a7377b49042b745503d0
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2021-12-29 23:07:11 +0530

    Add files via upload

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { screen } from '@testing-library/react';
 
 
 const Contact = () => {
@@ -76,22 +77,39 @@ alert("Plese Fill data")
 
 
   return (
-  
+  <div className=" h-screen w-full flex justify-center items-center"> 
 
-  
-    <div>
+ 
+
 
     <form method="POST">
 
-    <input name="name" autoComplete="off" placeholder='Enter your name...' value={user.name}  onChange={getUserData}  required />
-    <br />
-    <input name="text" autoComplete="off" placeholder='Messag...' value={user.text}  onChange={getUserData} />
-    <br />
-    <button onClick={postData}> Submit </button>
+
+      <div className="flex flex-col  space-y-5 p-5 bg-slate-100 border-2 rounded-md items-center "> 
+      <h1 className="font-semibold text-2xl">Contact Us </h1>
+
+    <input name="name" autoComplete="off" placeholder='Enter your name...' value={user.name}  onChange={getUserData} className="border-2 rounded-md px-3 py-2 " autoFocus/>
+    <textarea  name="text"  autoComplete="off" placeholder='' value={user.text}  onChange={getUserData} className="border-2 rounded-md px-5  py-2 " />
+    
+
+
+
+    <button onClick={postData} className="border-2 bg-cyan-300 text-white rounded-md px-7 py-2 "> Submit </button>
+    <h1 className="text-sm"> Developed By <a href="#" className="text-blue-500 font-semibold underline hover:cursor-pointer">  SAGAR </a> @2021</h1>
+    </div>
+
+   
     </form>
 
-    </div>
   
+
+
+
+    </div>
+
+
+
+
   )
 }
 
